191a9f0b48f06fb65e98ce45e73442a2adce891c
Sergio <principiante.en.programar@gmail.com>
2026-02-14 04:38:56 -0300

    change style bg

--- src/components/ClipViewer.tsx
+++ src/components/ClipViewer.tsx
@@ -50,7 +50,7 @@ export default function ClipViewer() {
         <div className="flex h-screen w-full items-center justify-center bg-transparent text-black dark:text-white font-semibold font-[Arial] uppercase text-shadow-black">
             {
                 clip && (
-                    <div className='flex flex-col items-center justify-center h-screen w-screen p-12'>
+                    <div className='flex flex-col items-center justify-center h-screen w-screen p-12 border-2 border-slate-950 rounded-lg bg-slate-950'>
                         <h1 className="text-6xl font-bold pb-10 bg-linear-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent">{clip.clipTitle}</h1>
                         <iframe
                             src={`https://clips.twitch.tv/embed?clip=${clip.clipId}&autoplay=true&parent=${hostname}&parent=streamelements.com&parent=localhost`}
